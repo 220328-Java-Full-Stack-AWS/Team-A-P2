aa8555daeb5135754580b55c9784cb1a25a7828c
Your Name <leobarrientos02@gmail.com>
2022-05-03 11:36:40 -0400

    product class

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,4 +1,22 @@
 package com.revature;
 
+import com.revature.Models.Product;
+import com.revature.Models.User;
+
+import java.text.DecimalFormat;
+
 public class Driver {
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
+    public static void main(String[] args){
+
+        Product product1 = new Product(1,"ToothPaste", 2.50, 20, "Toothpaste to brush your teeth", "ImageURL" );
+        Product product2 = new Product(2, "Towel", 5.00, 10, "Towel for the beach", "Image");
+        User user1 = new User(1,"user1", "user1@gmail.com", "password", "Leo", "Barrientos", "516-960-8000");
+        System.out.println("E-COMMERCE");
+
+        System.out.println(product1.getProductName() + " Price:" +  df.format(product1.getProductPrice())+ " Description: " + product1.getProductDescription());
+        System.out.println(product2.getProductName() + " Price:" + df.format(product2.getProductPrice()) + " Description: " + product2.getProductDescription());
+        System.out.println(user1);
+    }
 }
--- src/main/java/com/revature/Models/User.java
+++ src/main/java/com/revature/Models/User.java
@@ -1,4 +1,107 @@
 package com.revature.Models;
 
+import java.util.Objects;
+
 public class User {
+    private int userId;
+    private String username;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phone;
+
+    public User() {
+    }
+
+    public User(int userId, String username, String email, String password, String firstName, String lastName, String phone) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return userId == user.userId && Objects.equals(username, user.username) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(phone, user.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, username, email, password, firstName, lastName, phone);
+    }
+
+    @Override
+    public String toString() {
+        return "User:" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phone='" + phone;
+    }
 }
