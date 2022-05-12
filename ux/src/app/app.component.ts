5b19646ac355209248e0b38848ed2c3ef91dd38b
leobarrientos02 <leobarrientos02@gmail.com>
2022-05-12 02:14:03 -0400

    front end work

--- ux/src/app/app.component.ts
+++ ux/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ux';
+  title = 'ReValue';
+  public year = new Date().getFullYear();
+  public faUserAstronaut = faUserAstronaut;
 }
