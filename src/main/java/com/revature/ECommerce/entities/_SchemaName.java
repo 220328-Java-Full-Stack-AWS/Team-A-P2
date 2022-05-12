a7834fd7c4b05dc202cbb10377f93695035f9e8a
Jarod H <jarodheng@gmail.com>
2022-05-12 12:19:40 -0700

    Update _SchemaName.java

--- src/main/java/com/revature/ECommerce/entities/_SchemaName.java
+++ src/main/java/com/revature/ECommerce/entities/_SchemaName.java
@@ -5,6 +5,6 @@ package com.revature.ECommerce.entities;
  * For the schema to be added to, the schema with the specified name must exist in the database.
  */
 public class _SchemaName {
-    public final static String schemaName = "jh";
+    public final static String jarodSchema = "jh";
     public final static String publicSchema = "public";
 }
