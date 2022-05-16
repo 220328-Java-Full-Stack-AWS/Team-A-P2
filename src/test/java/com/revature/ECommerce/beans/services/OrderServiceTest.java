19d93e78703cf1578eb3e895503c7bc61e1fb4c8
Terrell Crawford <terrellcrawford05@gmail.com>
2022-05-16 15:06:01 -0700

    Initial commit

--- src/test/java/com/revature/ECommerce/beans/services/OrderServiceTest.java
+++ src/test/java/com/revature/ECommerce/beans/services/OrderServiceTest.java
@@ -1,12 +1,21 @@
 package com.revature.ECommerce.beans.services;
 
 import com.revature.ECommerce.beans.repositories.OrderRepository;
+import com.revature.ECommerce.entities.Order;
+import com.revature.ECommerce.entities.Product;
+import com.revature.ECommerce.entities.Sale;
+import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.sql.Timestamp;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderServiceTest {
@@ -21,7 +30,6 @@ public class OrderServiceTest {
 
     @BeforeAll
     public void runBeforeTestingStarts(){
-
     }
 
     @BeforeEach
@@ -34,39 +42,64 @@ public class OrderServiceTest {
     }
 
 
+    @Test
     public void addToOrderPassesWhenSaleIsSuccessfullyAdded(){
+        //Act
+        Timestamp timestamp= new Timestamp(1652735621);
+        Timestamp timestamp2 = new Timestamp(1652735625);
+        Order order= new Order();
+        Product p1 = new Product("PS5", 600.00, 100, "PlayStation 5", "ImgUrl", "In Stock", "Video Games");
+        Product p2 = new Product("XBOX Series X", 600.00, 100, "It's an XBOX", "ImgUrl", "In Stock", "Video Games");
+        Sale sale = new Sale(3,timestamp,p2);
+        Sale s2 = new Sale(50, timestamp2, p1);
+        order = oServ.addToOrder(order, s2);
+        when(mockOrderRepo.orderExists(order)).thenReturn(Boolean.FALSE);
+        //Assert
+        assertFalse(order.getSaleList().isEmpty());
+        assertEquals(s2, order.getSaleList().get(0));
+
 
     }
 
+    @Test
     public void removeFromOrderPassesWhenOrderHasSalesAndGivenSaleIsRemoved(){
 
     }
 
+    @Test
     public void removeFromOrderFailsWhenListOfSalesIsAlreadyEmpty(){
 
     }
 
+    @Test
     public void checkoutPassesWhenOrderIsAddedToUsersListOfOrders(){
 
     }
 
+    @Test
     public void checkoutFailsWhenUserDoesNotExist(){
 
     }
 
+    @Test
     public void testDelete(){
 
     }
 
+    @Test
     public void testGetById(){
 
     }
+
+    @Test
     public void testGetByUser(){
 
     }
+    @Test
     public void testGetAll(){
 
     }
+    @Test
     public void testUpdate(){
 
     }
