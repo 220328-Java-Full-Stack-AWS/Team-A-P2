624e8b9183972dc5489c4f40bf1fccf1542b2bab
leobarrientos02 <leobarrientos02@gmail.com>
2022-05-17 02:34:12 -0400

    front end new design

--- RevTech/src/app/app.component.ts
+++ RevTech/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { faUserAstronaut, faFaceRollingEyes, faCopyright, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit } from '@angular/core';
+import { faUserAstronaut, faFaceRollingEyes, faCopyright, faShoppingCart, faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +8,19 @@ import { faUserAstronaut, faFaceRollingEyes, faCopyright, faShoppingCart } from 
 })
 export class AppComponent {
   title = 'RevTech';
+  // Current yyear for nav bar
   public year = new Date().getFullYear();
     // Font Awesome Icons
     public faUserAstronaut = faUserAstronaut;
     public faFaceRollingEyes = faFaceRollingEyes;
     public faCopyright = faCopyright;
     public faShoppingCart = faShoppingCart;
+    public faCartPlus = faCartPlus;
 
-}
+    // Front end add to cart
+    public counter = 0;
+    public AddOneToCart(){
+      this.counter++;
+    }
+
+  }
