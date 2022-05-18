6a3b6978602c21ca3680f18b0d7f8415a0ffb913
Terrell Crawford <terrellcrawford05@gmail.com>
2022-05-18 16:59:22 -0700

    Returned the OrderServiceTest file that got deleted

--- RevTech/src/app/orders/orders.component.ts
+++ RevTech/src/app/orders/orders.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import { Order } from "../dto/order";
+import { Sale } from "../dto/sale";
+import { OrderService } from '../services/order.service';
+import { SaleService } from '../services/sale.service';
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +13,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  // fa icon
+  public faCartPlus = faCartPlus;
+  public orders!: Order[];
+
+  constructor(private orderService: OrderService, private salesService: SaleService) { }
 
   ngOnInit(): void {
+
+  }
+
+  public getAllOrders(): void {
+    this.orderService.getAllOrders().subscribe(
+      (response: Order[]) => {
+        this.orders = response;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.message)
+      }
+    )
+  }
+
+  public getOrderById(id: number) {
+
+  }
+
+  /*public addToUser(user: User) {
+
+  }*/
+
+  public addToOrder(sale: Sale) {
+
   }
 
 }
+
+
