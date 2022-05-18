1320f72bdf038fa138b16509acbd08704b718e8f
leobarrientos02 <leobarrientos02@gmail.com>
2022-05-18 17:29:58 -0400

    register form complete

--- RevTech/src/app/register-page/register-page.component.ts
+++ RevTech/src/app/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faFaceRollingEyes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-register-page',
@@ -12,4 +13,16 @@ export class RegisterPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  public faFaceRollingEyes = faFaceRollingEyes;
+  // variable - default false
+  show: boolean = false;
+  show2: boolean = false;
+  // click event function toggle
+  password() {
+    this.show = !this.show;
+  }
+  confirmPassword() {
+    this.show2 = !this.show2;
+  }
 }
