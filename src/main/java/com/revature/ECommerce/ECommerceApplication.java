f701fc788f8c6cd0f9493cf107ec05517fce833a
Jarod H <jarodheng@gmail.com>
2022-05-24 11:32:23 -0700

    cors again

--- src/main/java/com/revature/ECommerce/ECommerceApplication.java
+++ src/main/java/com/revature/ECommerce/ECommerceApplication.java
@@ -98,18 +98,17 @@ public class ECommerceApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://localhost:4200/%22",
-				"http://http://revtech-ui.s3-website-us-west-1.amazonaws.com", "http://http://revtech-ui.s3-website-us-west-1.amazonaws.com/%22",
-				"http://revtechteamap2-env-1.eba-x9awq5nc.us-west-1.elasticbeanstalk.com",
-				"http://revtechteamap2-env-1.eba-x9awq5nc.us-west-1.elasticbeanstalk.com/%22"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://revtech-ui.s3-website-us-west-1.amazonaws.com",
+				"http://revtechteamap2-env-1.eba-x9awq5nc.us-west-1.elasticbeanstalk.com", "*"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Accept", "Authorization", "X-Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers", "mode"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "mode"));
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "mode"));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
+
 }
--- src/main/java/com/revature/ECommerce/beans/controllers/CorsFilter.java
+++ src/main/java/com/revature/ECommerce/beans/controllers/CorsFilter.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
-/**
+/*
 @Component
 public class CorsFilter extends OncePerRequestFilter {
     @Override
